bf9e98750c98551946806b77a204b8287121337a
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-04-25 20:50:35 +0530

    output management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.innerHTML += myName("Abhishek");
+
+    btn.innerHTML = 'Click me and check console';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
   }
